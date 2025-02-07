2f47acd64ae07b2945a4284971b81e7cfc0a93a0
SRAVANTHI VADLA <137860943+Sravanthi2727@users.noreply.github.com>
2025-02-07 13:14:17 +0530

    Search College
    Dynamically updated. Need to work more.

---
+++ College Search/By Country/col.js
@@ -0,0 +1,33 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+
+let url = "http://universities.hipolabs.com/search?name=";
+
+btn.addEventListener("click", async () => {
+  let c = inp.value;
+  //console.log(c);
+  let colArr = await search(c);
+  names(colArr);
+});
+
+async function search(c) {
+  try {
+    let d = await axios.get(url + c);
+    console.log(d.data);
+    return d.data;
+  } catch (e) {
+    //console.log(e);
+    return [];
+  }
+}
+
+function names(colArr) {
+  let list = document.querySelector("#list");
+  list.innerText = "";
+  for (i of colArr) {
+    let li = document.createElement("li");
+    li.innerText = i.name;
+    list.appendChild(li);
+    //console.log(i);
+  }
+}
